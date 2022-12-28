4bac7d8600f412ef0a7b395c6427404d7a26bcd1
Juan Quino <quinojuan@gmail.com>
2022-12-28 17:39:05 -0300

    data.map

--- src/components/Informe.jsx
+++ src/components/Informe.jsx
@@ -10,14 +10,50 @@ import {
 } from "@mui/material";
 
 export default function Informe() {
-  const nombre = "Juan Quino";
+  const data = [
+    {
+      name: "Juan Carlos",
+      pub: 5,
+      vid: 10,
+      hs: 30,
+      rev: 7,
+      est: 2,
+    },
+    {
+      name: "Febe",
+      pub: 3,
+      vid: 15,
+      hs: 45,
+      rev: 2,
+      est: 4,
+    },
+    {
+      name: "Janet",
+      pub: 4,
+      vid: 3,
+      hs: 30,
+      rev: 7,
+      est: 2,
+    },
+    {
+      name: "María",
+      pub: 6,
+      vid: 15,
+      hs: 70,
+      rev: 17,
+      est: 3,
+    },
+  ];
   return (
     <div>
-      <p><strong>Nombre y apellido:</strong> {nombre}</p>
+      <p>
+        <strong>Nombre y apellido:</strong> ?{" "}
+      </p>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow style={{ background: "lightGrey" }}>
+              <TableCell align="center">Nombre</TableCell>
               <TableCell align="center">Pub</TableCell>
               <TableCell align="center">Vid</TableCell>
               <TableCell align="center">Hs</TableCell>
@@ -26,42 +62,18 @@ export default function Informe() {
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-            </TableRow>
-            <TableRow
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-            </TableRow>
-            <TableRow
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-            </TableRow>
-            <TableRow
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-            </TableRow>
+            {data.map((pub) => (
+              <TableRow
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell align="center">{pub.name}</TableCell>
+                <TableCell align="center">{pub.pub}</TableCell>
+                <TableCell align="center">{pub.vid}</TableCell>
+                <TableCell align="center">{pub.hs}</TableCell>
+                <TableCell align="center">{pub.rev}</TableCell>
+                <TableCell align="center">{pub.est}</TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
